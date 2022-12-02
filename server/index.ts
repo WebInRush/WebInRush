9e3b95c6bf0329d11913be4167e3765fe8a08da8
SUBID DAS <itsmesubid@gmail.com>
2022-12-03 01:09:39 +0530

    Basic setup

---
+++ server/index.ts
@@ -0,0 +1,10 @@
+import express from "express";
+const app = express();
+const port = 3000 || process.env.PORT;
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+app.listen(port, () => {
+  console.log("Express.js: Listening on port 3000!");
+});
