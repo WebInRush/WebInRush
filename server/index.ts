19ff0f47123306ed7d1ea014f6f0710ed3ed6725
SUBID DAS <itsmesubid@gmail.com>
2022-12-04 02:16:20 +0530

    Minor Updates

--- server/index.ts
+++ server/index.ts
@@ -1,7 +1,10 @@
 import express from "express";
 import mysql from "mysql";
+import cors from "cors";
+require("dotenv").config();
+
 const app = express();
-const port = 8800 || process.env.PORT;
+const port = process.env.PORT || 3000;
 
 const db = mysql.createConnection({
   host: "localhost",
@@ -10,19 +13,33 @@ const db = mysql.createConnection({
   database: "test",
 });
 
-// If there is an auth problem 
+// If there is an auth problem
 // ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY '12345'
+
+app.use(cors());
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
-app.get("/books", (req, res) => {
-  const sql = "SELECT * FROM books";
-  db.query(sql, (err, result) => {
-    if (err) throw err;
-    res.send(result);
-  });
-});
+
+// app.get("/books", (req, res) => {
+//   const sql = "SELECT * FROM books";
+//   db.query(sql, (err, result) => {
+//     if (err) throw err;
+//     res.send(result);
+//   });
+// });
+
+// app.post("/books", (req, res) => {
+//   const sql = "INSERT INTO books (`title`, `desc`, `cover`) VALUES (?)";
+//   const book = [[req.body.title, req.body.desc, req.body.cover]];
+//   db.query(sql, book, (err, result) => {
+//     if (err) throw err;
+//     res.json("Book added");
+//   });
+// });
+
 app.listen(port, () => {
-  console.log(`Express.js: Listening on port ${port}!`);
+  console.log(`Express.js: Listening on port https://localhost:${port}!`);
 });
